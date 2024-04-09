51dc95adc9778c04c8003a851ba734279a22cc18
20ballovEGE <124138369+20ballovEGE@users.noreply.github.com>
2024-04-09 11:39:17 +0300

    Add files via upload

---
+++ eugenpro/js/app.js
@@ -0,0 +1,13 @@
+// const burger_button = document.querySelector('.burger_button');
+// const menu = document.querySelector('.menu');
+// const close = document.querySelector('.close');
+
+
+// burger_button.addEventListener('click', function(){
+//     menu.classList.add('active')
+// });
+
+// close.addEventListener('click', function(){
+//     menu.classList.remove('active')
+// });
+ 
